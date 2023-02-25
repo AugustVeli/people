b8b189c7bc4a822b3140164e977154f663e5c49b
Artyom Ustinov <ustinovartyom@gmail.com>
2023-02-25 22:03:16 +0200

    try

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $base_path = __DIR__ . '//';
 
 function my_autoloader(string $class_name){
     global $base_path;
-    require_once $base_path . $class_name . '.php'; ;
+    require_once $base_path . $class_name . '.php'; 
 }
 
 spl_autoload_register('my_autoloader');
